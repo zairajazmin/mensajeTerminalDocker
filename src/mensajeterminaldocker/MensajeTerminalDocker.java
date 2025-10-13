33f21ffb7a283569a7fca3cd12bdbca5d8d409ab
Zaira Jazmin <zairajazmin566@gmail.com>
2025-10-12 22:45:28 -0600

    Se agrega el proyecto

---
+++ src/mensajeterminaldocker/MensajeTerminalDocker.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mensajeterminaldocker;
+
+/**
+ *
+ * @author zaira
+ */
+public class MensajeTerminalDocker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("¡Hola desde Docker!");
+        // TODO code application logic here
+    }
+    
+}
